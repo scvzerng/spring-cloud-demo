3702efcde2b66885c3b1e5abb882bcef985b6ada
scvzerng <443683059a>
2017-09-12 16:57:43 +0800

    spring cloud bus kafka

--- service_consumer/src/main/java/com/yazuo/xiaoya/service/ConsumerController.java
+++ service_consumer/src/main/java/com/yazuo/xiaoya/service/ConsumerController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -16,6 +18,7 @@ import javax.annotation.Resource;
  * Created by scvzerng on 2017/9/4.
  */
 @RestController
+@RefreshScope
 public class ConsumerController {
     @Value("${test}")
     private String test;
@@ -28,7 +31,10 @@ public class ConsumerController {
     public JSONObject test(){
         return testService.getTestInfo();
     }
-
+    @GetMapping("/props/{key}")
+    public String getProps(@PathVariable String key){
+        return test;
+    }
     public JSONObject errorTest(){
         JSONObject json = new JSONObject();
         json.put("status","error");
